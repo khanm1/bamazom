51fcec9a7bb329b0ea4ede7014f7c64244230197
mkhan <riponk9519@gamil.com>
2019-01-02 20:26:23 -0600

    Modified code to bamazonCustomer.js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,11 +6,11 @@ var chalk = require("chalk");
 var asTable = require("as-table");
 
 var connection = mysql.createConnection({
-    host: process.env.DB_LOCALHOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: "localhost",
+    port: 3306,
+    user: "Mohammad",
+    password: "Password",
+    database: "bamazon"
 });
 
 function start(){
