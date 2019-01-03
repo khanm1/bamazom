56a3c58a05e05138fde428fe884a68241f056f79
mkhan <riponk9519@gamil.com>
2019-01-02 20:31:15 -0600

    Modified code in bamazonManager.js file

--- bamazonManager.js
+++ bamazonManager.js
@@ -7,11 +7,11 @@ var asTable = require("as-table");
 
 var connection = mysql.createConnection(
     {
-    host: process.env.DB_LOCALHOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+        host: "localhost",
+        port: 3306,
+        user: "Mohammad",
+        password: "Password",
+        database: "bamazon"
     }
 );
 
